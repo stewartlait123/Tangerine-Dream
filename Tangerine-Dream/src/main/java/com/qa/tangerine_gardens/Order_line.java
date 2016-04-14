c663643e01f69d51e44d0e180f54a7087df15cd4
Chrissystk <christine.stokes@qa.com>
2016-04-14 16:07:43 +0100

    commiting for pull

---
+++ Tangerine-Dream/src/main/java/com/qa/tangerine_gardens/Order_line.java
@@ -0,0 +1,44 @@
+package com.qa.tangerine_gardens;
+
+import java.math.BigInteger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+@Entity
+@Table (name="order_line")
+public class Order_line {
+	@Id
+	@Column (name = "qty", nullable = false, length = 10)
+	@NotNull
+	@Size (min = 1, max = 10)
+	private int qty;
+	
+	@OneToOne
+	@JoinColumn(name = "product_id_fk", nullable = false)
+	@NotNull
+	private Product product_id_fk;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id_fk", nullable = false)
+	@NotNull
+	private Order order_id_fk;
+	
+	public Order_line(){}
+	
+	public int getQty() {
+		return qty;
+	}
+	
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	
+}
