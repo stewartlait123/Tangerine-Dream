7c2903ca9f737436453634221f52d0fa200ac564
Chrissystk <christine.stokes@qa.com>
2016-04-14 16:27:11 +0100

    James' quote pull always but only push in love ... errors in code now sorted

--- src/main/java/com/qa/tangerine_gardens/entities/Product.java
+++ src/main/java/com/qa/tangerine_gardens/entities/Product.java
@@ -1,5 +1,13 @@
-package com.qa.Tangerine_Gardens.Data;
+package com.qa.tangerine_gardens.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 @Entity
 @Table (name = "product")
 
