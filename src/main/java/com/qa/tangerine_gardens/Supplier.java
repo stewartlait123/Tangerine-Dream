e1699a2d95faf2fb50f614518b856aafa26cbeb0
IainQuate <iainquate@hotmail.co.uk>
2016-04-14 15:57:46 +0000

    Added Supplier, OrderLine, PurchaseOrderLine. Fixed Payment.

---
+++ src/main/java/com/qa/tangerine_gardens/Supplier.java
@@ -0,0 +1,29 @@
+package com.qa.tangerine_gardens;
+
+import java.math.BigInteger;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table (name="supplier")
+
+public class Supplier {
+
+	@Id
+	@Column (name = "supplier_id")
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private BigInteger supplier_id;
+	
+	
+	public Supplier(){}
+
+	public BigInteger getSupplier_id() {
+		return supplier_id;
+	}
+
+	
+}
