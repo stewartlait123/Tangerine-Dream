83e8e818385a3a0a3ea565b0a52a43a1f79382ac
Finley <FinnDevelop@gmail.com>
2016-04-15 11:12:21 +0000

    Created AddressRepository interface class

---
+++ src/main/java/com/qa/tangerine_gardens/entities/AddressRepository.java
@@ -0,0 +1,32 @@
+package com.qa.tangerine_gardens.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface AddressRepository {
+	
+	public void persisBillingAddress (Address a);
+	public void persistBillingAddresses(List<Address> a);
+	
+	public void persisDeliveryAddress (Address a);
+	public void persistDeliveryAddresses(List<Address> a);
+	
+	public Address findBillingAddress(String s);
+	public ArrayList<Address> getBillingAddresses();
+	
+	public Address findDeliveryAddress(String s);
+	public ArrayList<Address> getDeliveryAddresses();
+	
+	public void addBillingAddress(Address a);
+	
+	public void addDeliveryAddress(Address a);
+	
+	public void removeBillingAddress(Address a);
+	
+	public void removeDeliveryAddress(Address a);
+	
+	public void updateBillingAddress(Address a);
+	
+	public void updateDeliveryAddress(Address a);	
+	
+}
