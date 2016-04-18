d529dbb17c339e0aaaca460e5edd839087e4d296
jmaddocks <jessica.maddocks.qa.com>
2016-04-18 17:17:16 +0100

    incomplete orderline entity

--- src/main/java/entities/OrderLine.java
+++ src/main/java/entities/OrderLine.java
@@ -1,5 +1,12 @@
 package entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * 
  * @author Jessica Maddocks 
@@ -8,15 +15,19 @@ package entities;
  * Christine Stokes created version 1.0.
  */
 
+@Entity
+@Table (name = "OrderLines")
 public class OrderLine {
-	
+	@Id 
+	@Column (name = "order")
+	@GeneratedValue	(strategy = GenerationType.IDENTITY)//*****************continue from here
 	private Order order;
 	private Product product;  
 	private int quantity;
 	private int purchasePrice;
 	
 	//constructors for attributes
-	public void OrderLine (Order order, Product product, int quantity, int purchasePrice) {
+	public OrderLine (Order order, Product product, int quantity, int purchasePrice) {
 	
 		this.order = order;
 		this.product = product;
@@ -33,45 +44,38 @@ public class OrderLine {
 	
 	public Product getproduct(){
 		
-		return product;
-	
+		return product;	
 	}
 
 	public int getquantity(){
 		
-		return quantity; 
-		
+		return quantity; 		
 	}
 	
 	public int getpurchasePrice(){
 		
-		return purchasePrice;
-		
+		return purchasePrice;		
 	}
 	
 	//attributes setters
 	public void setorder(Order order){
 		
-		this.order = order;  
-		
+		this.order = order;  		
 	}
 	
 	public void setproduct(Product product){
 		
-		this.product = product;
-	
+		this.product = product;	
 	}
 
 	public void setquantity(int quantity){
 		
-		this.quantity = quantity; 
-		
+		this.quantity = quantity; 		
 	}
 	
 	public void setpurchasePrice(int purchasePrice){
 		
-		this.purchasePrice = purchasePrice;
-		
+		this.purchasePrice = purchasePrice;		
 	}
 	
 }
