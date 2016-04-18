64e3675c6368e21e73b8806411564d9c99afeda0
Mumar1 <m.u12@hotmail.co.uk>
2016-04-18 14:58:02 +0100

    Added implementation to methods in POL repo class

--- src/main/java/implementations_offline/PurchaseOrderLineRepositoryOffline.java
+++ src/main/java/implementations_offline/PurchaseOrderLineRepositoryOffline.java
@@ -15,32 +15,65 @@ public class PurchaseOrderLineRepositoryOffline implements PurchaseOrderLineRepo
 	@Inject
 	private InitialData initialData;
 	
+	/*
+	 * Add purchase order line*/
 	public void persistPurchaseOrderLine(PurchaseOrderLine purchaseOrderLine){
 		initialData.addPurchaseOrderLine(purchaseOrderLine);
 	}
 	
+	/*
+	 * Find a purchase order line by Id*/
 	@Override
 	public PurchaseOrderLine findByPurchaseOrderLineIdSingle(long id){
+		ArrayList<PurchaseOrderLine> purchaseOrderLineList = initialData.getPurchaseOrderLines();
+		
+		for (int i = 0; i < purchaseOrderLineList.size(); i++){
+			if (purchaseOrderLineList.get(i).getPurchaseOrderlineId() == id){
+				return purchaseOrderLineList.get(i);
+			}
+		}
 		return null;
 	}
 	
+	/*
+	 * Get Purchase Order Lines */
 	@Override
 	public ArrayList<PurchaseOrderLine> getPurchaseOrderLines(long id){
-		return null;
+		return initialData.getPurchaseOrderLines();
 	}
 	
+	/*
+	 * Update Purchase Order Line*/
 	@Override
 	public void updatePurchaseOrderLine(PurchaseOrderLine p){
+		ArrayList<PurchaseOrderLine> purchaseOrderLineList = initialData.getPurchaseOrderLines();
 		
+		for (int i = 0; i < purchaseOrderLineList.size(); i++) {
+			if (purchaseOrderLineList.get(i).getPurchaseOrderlineId() == p.getPurchaseOrderlineId()){
+				purchaseOrderLineList.set(i, p);
+				initialData.setPurchaseOrderLines(purchaseOrderLineList);
+				break;
+			}
+		}
 	}
 	
+	/*
+	 * Remove Purchase Order Line*/
 	@Override
 	public void removePurchaseOrderLine(PurchaseOrderLine p){
+		ArrayList<PurchaseOrderLine> purchaseOrderLineList = initialData.getPurchaseOrderLines();
 		
+		for (int i = 0; i < purchaseOrderLineList.size(); i++){
+			if (purchaseOrderLineList.get(i).getPurchaseOrderlineId() == p.getPurchaseOrderlineId()){
+				purchaseOrderLineList.remove(i);
+			}
+		}
 	}
 	
+	/*
+	 * Add Purchase Order Line*/
 	@Override
 	public void addPurchaseOrderLine(PurchaseOrderLine purchaseOrderLine){
-		
+		initialData.addPurchaseOrderLine(purchaseOrderLine);
 	}
 }
