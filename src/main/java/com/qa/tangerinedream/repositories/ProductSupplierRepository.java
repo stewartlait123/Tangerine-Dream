0d69b1aed472f30dd4f318ee3b871eb700c02120
IainQuate <iainquate@hotmail.co.uk>
2016-04-19 12:11:09 +0000

    Updated ProductSupplierRepository and ProductSupplierRepositoryOffline to have matching methods.

--- src/main/java/com/qa/tangerinedream/repositories/ProductSupplierRepository.java
+++ src/main/java/com/qa/tangerinedream/repositories/ProductSupplierRepository.java
@@ -1,7 +1,6 @@
 package com.qa.tangerinedream.repositories;
 
 import java.util.ArrayList;
-import java.util.Currency;
 import java.util.List;
 
 import com.qa.tangerinedream.entities.ProductSupplier;
@@ -13,8 +12,6 @@ public interface ProductSupplierRepository {
 	public ProductSupplier findById(int id);
 	public void removeProductSupplier (ProductSupplier p);
 	public ArrayList<ProductSupplier> getProductSuppliers();
-	public void updateCost(Currency c);
-	public ArrayList<ProductSupplier> getProductSupplier(ProductSupplier ps);
 	void updateProductSupplier(ProductSupplier ps);
 
 }
