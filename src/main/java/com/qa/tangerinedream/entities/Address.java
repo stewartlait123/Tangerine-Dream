264fc27910222ed6688d342193394f5e48333a85
Finley <FinnDevelop@gmail.com>
2016-04-19 15:18:29 +0000

    Completed constructor in Address.java. Requires go ahead from team lead to resolve issues. See Jira Issue TAG-37

--- src/main/java/com/qa/tangerinedream/entities/Address.java
+++ src/main/java/com/qa/tangerinedream/entities/Address.java
@@ -36,8 +36,10 @@ public class Address {
 	long customer_id_fk;
 	
 	
-	public Address(String string, String string2, Customer customer_ID) {
+	public Address(String deliveryAddress ,String billingAddress, Long customer_ID) {
 		// TODO Auto-generated constructor stub
+		
+		
 	}
 
 	public String getBilling_address() {
