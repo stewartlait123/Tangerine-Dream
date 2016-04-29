58125391d1136430343a903bad082cfe6e70914f
Finley <FinnDevelop@gmail.com>
2016-04-29 15:34:10 +0000

    Began work on integrating controller with homepage. Began Payment service

--- src/main/java/com/qa/tangerinedream/service/PaymentService.java
+++ src/main/java/com/qa/tangerinedream/service/PaymentService.java
@@ -1,5 +1,50 @@
+/*
+ *  @Author Finley Peters Team Tang 2016
+ */
+
 package com.qa.tangerinedream.service;
 
-public class PaymentService {
+import javax.inject.Inject;
 
+import com.qa.tangerinedream.controllers.BasketController;
+import com.qa.tangerinedream.controllers.CurrentUser;
+import com.qa.tangerinedream.repositories.CustomerRepository;
+import com.qa.tangerinedream.repositories.OrderRepository;
+
+public class PaymentService {
+	@Inject OrderRepository orderRepository;
+	@Inject CustomerRepository customerRepository;
+	@Inject BasketController basketController;
+	@Inject CurrentUser currentUser;
+	
+	
+	
+	public void makeCardPayment()
+	{
+		//TODO add card payment logic.
+		/**
+		 * if(cardPaymentLogic/Method)
+		 * {
+		 * basketController.placeOrder();
+		 *  }else
+		 *  {
+		 *  	inform user payment failed/send user to payment failed page
+		 *  }
+		 * 
+		 * On a true return from the card payment process, placeOrder
+		 * Until the functionality to charge a card is in place, assume
+		 * the payment has been successful and just place the order, as below. 
+		 */
+		
+		basketController.placeOrder();
+	}
+	
+	public void makeCreditPayment()
+	{
+		//currentUser.getUserID()
+		customerRepository.updateCredit( currentUser.getUserID(), (int) basketController.getTotalPrice());
+		//TODO continue from here
+	}
+	
+	
 }
