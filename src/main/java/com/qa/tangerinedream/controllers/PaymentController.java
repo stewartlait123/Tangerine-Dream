fb14b40c01adb12a64440cd3424b414aea443cab
Chrissystk <christine.stokes@qa.com>
2016-05-04 14:20:12 +0100

    PaymentController done and xhtml edited

--- src/main/java/com/qa/tangerinedream/controllers/PaymentController.java
+++ src/main/java/com/qa/tangerinedream/controllers/PaymentController.java
@@ -5,11 +5,12 @@ import javax.inject.Inject;
 import javax.inject.Named;
 /*
  * 
- *  @author Finley Peters, TeamTang 2016
+ *  @author Christine Stokes, TeamTang 2016
  * 
  */
 
-import com.qa.tangerinedream.service.LoggedInUser;
+
+import com.qa.tangerinedream.service.OrderService;
 import com.qa.tangerinedream.service.PaymentService;
 
 
@@ -19,17 +20,77 @@ import com.qa.tangerinedream.service.PaymentService;
 public class PaymentController {
 
 	@Inject PaymentService paymentService;
-	@Inject LoggedInUser loggedInUser;
+	@Inject CurrentUser currentUser;
+	@Inject OrderService orderservice;
 	
+	private String nameOnCard;
+	private String cardNumber;
+	private String expiryDate;
+	private String cSV;
 	
-/**	public Boolean payByCard()
-	{
-		return false; //Returns a boolean for if the transaction succeeds or not
+	private String method;
+	
+	/* Setter for Paying by Card */
+
+	public String Submit(){
+		if (method.equalsIgnoreCase("2")){
+			if (nameOnCard.isEmpty() || cardNumber.isEmpty() || expiryDate.isEmpty() || cSV.isEmpty())
+				return "payment";
+			else {
+				paymentService.paybycard(nameOnCard, cardNumber, expiryDate, cSV);
+			return "orderconfirmed";
+		}
+		}
+		else if (method.equalsIgnoreCase("3")){
+			boolean accepted = false;
+			paymentService.creditpayment(currentUser.getUserID(), accepted);
+			
+		return "orderconfirmed";
+		}
+		else
+			return "payment";
 	}
-		
-	public Boolean payByCredit(loggedInUser.getID)
-	{
-		return false; //Returns a boolean for if the transaction succeeds or not
-	}	**/
+	
+
+	public void setNameOnCard(String nameOnCard) {
+		this.nameOnCard = nameOnCard;
+	}
+	public void setCardNumber(String cardNumber) {
+		this.cardNumber = cardNumber;
+	}
+	public void setExpiryDate(String expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+	public void setcSV(String cSV) {
+		this.cSV = cSV;
+	}
+	public PaymentService getPaymentService() {
+		return paymentService;
+	}
+	public CurrentUser getCurrentUser() {
+		return currentUser;
+	}
+	public String getNameOnCard() {
+		return nameOnCard;
+	}
+	public String getCardNumber() {
+		return cardNumber;
+	}
+	public String getExpiryDate() {
+		return expiryDate;
+	}
+	public String getcSV() {
+		return cSV;
+	}
+	
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+	
+
 
 }
