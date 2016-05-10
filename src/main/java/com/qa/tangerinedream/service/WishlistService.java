2edaf7ddf69c95f0c32db845ad15763fc6ffef3e
unknown <stewartlait123@gmail.com>
2016-05-10 15:36:31 +0100

    Updates

--- src/main/java/com/qa/tangerinedream/service/WishlistService.java
+++ src/main/java/com/qa/tangerinedream/service/WishlistService.java
@@ -54,7 +54,7 @@ public class WishlistService {
 
 	public List<OrderLine> getWishlist(long userID) {
 		Order order = orderRepository.findUserAndStatus(userID, OrderStatus.WISHLIST);
-		System.out.println(order.toString());	
+		System.out.println(order.getOrderLines().toString());	
 		return order.getOrderLines(); 
 	}
 }
