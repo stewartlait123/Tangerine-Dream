73bb319c7b80ca2b17458b09d545ae40c61e4a6c
jmaddocks <jessica.maddocks@qa.com>
2016-05-12 16:06:12 +0100

    add card has a page, fields to add details, buttons but currently no place to save details to

---
+++ src/main/java/com/qa/tangerinedream/service/AddCardService.java
@@ -0,0 +1,12 @@
+package com.qa.tangerinedream.service;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class AddCardService {
+
+	public void addCard(String nameOnCard, String cardNumber, String type, String expiryDate, String cSV) {
+		// TODO Auto-generated method stub
+		System.out.println("--Gnome Life Bitcheaz!--");
+	}
+}
