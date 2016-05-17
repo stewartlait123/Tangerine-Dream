b80f24a37cc3081b41df607f658b23206a09dca7
Finley <FinnDevelop@gmail.com>
2016-05-17 10:41:35 +0000

    Stashing changes to CustomerTest

--- src/main/java/com/qa/tangerinedream/entities/CustomerTest.java
+++ src/main/java/com/qa/tangerinedream/entities/CustomerTest.java
@@ -100,7 +100,6 @@ public class CustomerTest {
 		try {
 
 			date = formatter.parse(dateInString);
-			System.out.println(date);
 			System.out.println(formatter.format(date));
 
 		} catch (ParseException e) {
